038b4a3795168b683b6de9a3e4b019e779773c56
Jack Paladin <jack.a.paladin@gmail.com>
2017-02-22 12:33:11 -0600

    Add getIngredients

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ def getRecipeByIngredients(ingredientList, fillIngredients=False, limitLicense=F
 	fillIngredientsString = "fillIngredients=" + str(fillIngredients) + "&"
 	
 	ingredientString = "ingredients="
+	
 	for i in range(len(ingredientList)):
 		if i == len(ingredientList) - 1:
 			ingredientString = ingredientString + ingredientList[i] + "&"
@@ -13,9 +14,22 @@ def getRecipeByIngredients(ingredientList, fillIngredients=False, limitLicense=F
 	numRecipesString = "number=" + str(numRecipes) + "&"
 	rankingString = "ranking=" + str(ranking)
 	url = url + fillIngredientsString + ingredientString + limitLicenseString + numRecipesString + rankingString
-	print url
+	
 	response = unirest.get(url, headers={
 		"X-Mashape-Key": "bojeGw3FjNmsh7gaFMMHPo1p3r5Lp1kgmMojsnVKq9kwB6d9az",
     	"Accept": "application/json"
 	})
+
+	return response
+
+def getIngredients(recipeId, includeNutrition=False):
+	url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" 
+	url = url + str(recipeId) 
+	url = url + "/information?includeNutrition=" + str(includeNutrition)
+	
+	response = unirest.get(url, headers={
+		"X-Mashape-Key": "bojeGw3FjNmsh7gaFMMHPo1p3r5Lp1kgmMojsnVKq9kwB6d9az",
+    	"Accept": "application/json"
+	})
+	
 	return response
