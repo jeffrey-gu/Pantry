c1d9b4426e33b750b242c6118211b086203747c2
Cindy Chong <cindy.chong@wustl.edu>
2017-02-27 20:44:23 -0600

    add ionic test app files

---
+++ ionic-test/src/pages/about/about.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  //added this
+  public swipe: number = 0;
+  
+  constructor(public navCtrl: NavController) {
+
+  }
+  //added this
+  swipeEvent(e){
+    this.swipe++;
+  }
+
+}
