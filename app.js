8ea01ae7638b8f5d567fe6f4babca237cbac8f9f
Pantry <pantry@ip-172-31-23-192.us-west-2.compute.internal>
2017-03-19 19:35:14 +0000

    edited app.js to include a GET request ('hello world') as well as a MySQL request to the console

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var express = require('express'),
     app = express();
+
+app.get('/', function(req, res){
+    res.send("Hello world!");
+});
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'pantry',
+  password : 'yummytummy',
+  database : 'pantry'
+});
+
+connection.connect()
+
+connection.query('SELECT * from users', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('First user is: ', rows[0].first_name)
+})
+
+connection.end()
+
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
