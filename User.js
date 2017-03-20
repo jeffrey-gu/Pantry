735bca2990f103562d88cf5ca30035af1f393bc8
Jack Paladin <jack.a.paladin@gmail.com>
2017-03-19 22:16:17 -0500

    Add User

--- User.js
+++ User.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
 	host     : 'localhost',
-	user     : 'pantry'
-	password : 'yummytummy'
+	user     : 'pantry',
+	password : 'yummytummy',
 	database : 'pantry'
 })
 
