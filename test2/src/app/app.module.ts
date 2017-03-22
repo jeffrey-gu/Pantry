24521c2572531fcbbe98ef85b280253fe8b299e7
Phoebe Moh <m>
2017-03-22 14:23:48 -0500

    Added pull up tab (still visually glitchy)

--- test2/src/app/app.module.ts
+++ test2/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { IonPullupModule } from 'ionic-pullup';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,{tabsPlacement:'top'}),
+    IonPullupModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -25,6 +28,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     HomePage,
     TabsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {}
--- test2/src/pages/home/home.ts
+++ test2/src/pages/home/home.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {IonPullUpComponent, IonPullUpFooterState} from 'ionic-pullup';
+import {IonPullUpTabComponent} from 'ionic-pullup-tab';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    footerState: IonPullUpFooterState;
 
-  constructor(public navCtrl: NavController) {
+          constructor(public navCtrl: NavController) {
+            this.footerState = IonPullUpFooterState.Collapsed;
+          }
 
-  }
+          footerExpanded() {
+            console.log('Footer expanded!');
+          }
+
+          footerCollapsed() {
+            console.log('Footer collapsed!');
+          }
+
+          toggleFooter() {
+            this.footerState = this.footerState == IonPullUpFooterState.Collapsed ? IonPullUpFooterState.Expanded : IonPullUpFooterState.Collapsed;
+          }
+
 
 }
