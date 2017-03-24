2ed5cbb8b36e5ddb7b79f669911595ff9c3716dc
Phoebe Moh <m>
2017-03-23 21:18:28 -0500

    -Added camera access! - Phoebe

--- test2/src/pages/about/about.ts
+++ test2/src/pages/about/about.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {Camera} from 'ionic-native';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  public base64Image: string;
+  
   constructor(public navCtrl: NavController) {
-
   }
-
+  
+ takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
 }
