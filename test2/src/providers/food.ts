3bef14dba3056efb7ce43a25e6f08c0d5c92c10e
Phoebe Moh <m>
2017-04-05 11:10:09 -0500

    -Phoebe

--- test2/src/providers/food.ts
+++ test2/src/providers/food.ts
@@ -10,12 +10,32 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Food {
 
-	public foodthings = [];
+	public foodthings= [];
 	
 	constructor() {
+    interface food {
+        name: string;
+        imageURL: string;
+        selected:  boolean;
+        recipeSelected: boolean;
+        pantrySelected:  boolean;
+      }
+      
+      function pantryRequestListener () {
+      this.foodthings= JSON.parse(this.responseText)['food'];
+      console.log("one");
+      console.log(this.foodthings[0].name);
+      console.log("two");
+      }
+       var request = new XMLHttpRequest();
+       
+    request.onload = pantryRequestListener;
+    request.open("get", '../testpantry.json', true);
+    request.send();
 		//GET REQUEST
-		this.foodthings = [{title:"pickle"},{title:"chicken"},{title:"bread"},{title:"eggs"},{title:"cheese"}];
+		//this.foodthings = [{title:"pickle"},{title:"chicken"},{title:"bread"},{title:"eggs"},{title:"cheese"}];
 		for(var i in this.foodthings){
+      console.log(this.foodthings[i].name);
 			this.foodthings[i]['recipeSelected'] = false;
 			this.foodthings[i]['pantrySelected'] = false;
 		}
